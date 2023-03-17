4aead5c1ef9acd26b04117853e3012408b5421aa
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-17 17:52:59 +0500

    main setup file added

---
+++ server.js
@@ -0,0 +1,12 @@
+import app from "./app.js";
+
+import { connectDb } from "./config/database.js";
+connectDb();
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>working fine</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server run");
+});
